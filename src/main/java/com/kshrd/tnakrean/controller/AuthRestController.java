3ea8c7c3b86a7ad64c4857c122f22d684f5a66fd
BoraUn.BoraUn <BoraUn.bora.un2000@gmail.com>
2022-06-05 08:37:44 +0700

    update user password

--- src/main/java/com/kshrd/tnakrean/controller/AuthRestController.java
+++ src/main/java/com/kshrd/tnakrean/controller/AuthRestController.java
@@ -1,9 +1,11 @@
-package com.kshrd.tnakrean.controller.dto;
+package com.kshrd.tnakrean.controller;
 
 import com.kshrd.tnakrean.configuration.security.JwtTokenUtil;
 import com.kshrd.tnakrean.model.apiresponse.ApiResponse;
 import com.kshrd.tnakrean.model.user.request.UserLoginRequest;
+import com.kshrd.tnakrean.model.user.request.UserUpdatePasswordRequestModel;
 import com.kshrd.tnakrean.model.user.response.AppUserResponse;
+import com.kshrd.tnakrean.repository.AppUserRepository;
 import com.kshrd.tnakrean.service.serviceImplementation.UserServiceImp;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +34,16 @@ public class AuthRestController {
 
     ModelMapper modelMapper = new ModelMapper();
 
+
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    static int user_id;
     @PostMapping("/login")
     ResponseEntity<AppUserResponse> login(@RequestBody UserLoginRequest request) {
 
@@ -46,14 +61,28 @@ public class AuthRestController {
         AppUserResponse response = modelMapper.map(findUserByUsername, AppUserResponse.class);
         response.setToken(token);
         System.out.println(response.getRole());
+        user_id = response.getId();
         return ResponseEntity.ok(response);
 
     }
 
-    @PostMapping("/edit")
-    public ResponseEntity<String> createUser(@RequestBody AppUserResponse appUserResponse){
-        userServiceImp.resetPassword(appUserResponse);
-        return ResponseEntity.ok().body("testing success");
+
+    @PostMapping("/resetpassword/")
+    public Object resetPassword(@RequestBody UserUpdatePasswordRequestModel userUpdatePasswordRequestModel){
+//        System.out.println("user_id"+user_id);
+        System.out.println("old:"+userUpdatePasswordRequestModel.getOld_password()+"new"+userUpdatePasswordRequestModel.getNew_password());
+        ApiResponse<String> response = new ApiResponse<>();
+        String password = appUserRepository.getPassword(user_id);
+//        System.out.println("getpass "+password);
+        boolean is_match = passwordEncoder.matches(userUpdatePasswordRequestModel.getOld_password(),password);
+        if(is_match){
+            String new_pass = passwordEncoder.encode(userUpdatePasswordRequestModel.getNew_password());
+            userServiceImp.resetPassword(new_pass,user_id);
+           return ApiResponse.updateSuccess();
+        }
+        else {
+            return "Miss Match";
+        }
     }
 
 
--- src/main/java/com/kshrd/tnakrean/service/serviceImplementation/UserServiceImp.java
+++ src/main/java/com/kshrd/tnakrean/service/serviceImplementation/UserServiceImp.java
@@ -3,10 +3,10 @@ package com.kshrd.tnakrean.service.serviceImplementation;
 import com.kshrd.tnakrean.repository.AppUserRepository;
 import com.kshrd.tnakrean.service.serviceInter.UserService;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class UserServiceImp implements UserService {
@@ -24,8 +24,8 @@ public class UserServiceImp implements UserService {
         return appUserRepository.loginByUserName(username);
     }
     @Override
-    public void resetPassword(AppUserResponse appUserResponse) {
-        appUserRepository.editPassword(appUserResponse);
+    public void resetPassword(String new_password,int user_id) {
+        appUserRepository.editPassword(new_password,user_id);
     }
 
 
