1920babd4d7c47fbabfd4772a0ef9ebd57c150f3
keomhab <keomhab@gmail.com>
2022-06-13 16:49:00 +0700

    Class Materials Type

---
+++ src/main/java/com/kshrd/tnakrean/model/classmaterials/request/ClassMaterialsTypeRequest.java
@@ -0,0 +1,12 @@
+package com.kshrd.tnakrean.model.classmaterials.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClassMaterialsTypeRequest {
+    String type;
+}
