fd1c1bf7c724cd4884dfd771871bd1e715f019f5
Chea Phanith <cheaphanith359@gmail.com>
2022-06-18 21:21:17 +0700

    push request notification

---
+++ src/main/java/com/kshrd/tnakrean/model/classmaterials/response/ClassResponse.java
@@ -0,0 +1,14 @@
+package com.kshrd.tnakrean.model.classmaterials.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClassResponse {
+    Integer created_by;
+    String name;
+}
--- src/main/java/com/kshrd/tnakrean/repository/OneSignalPushNotificationRepository.java
+++ src/main/java/com/kshrd/tnakrean/repository/OneSignalPushNotificationRepository.java
@@ -1,8 +1,28 @@
 package com.kshrd.tnakrean.repository;
 
+import com.kshrd.tnakrean.model.classmaterials.response.ClassResponse;
+import com.kshrd.tnakrean.model.classmaterials.response.NotificationResponse;
+import com.kshrd.tnakrean.model.student.request.StudentRequest;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface OneSignalPushNotificationRepository {
+
+    @Insert("INSERT INTO reqee ")
+    Boolean studentRequestToJoinClass(StudentRequest studentRequest);
+
+    @Select("SELECT")
+    Boolean checkingStudentAlreadyRequest(Integer userId);
+
+    @Select("SELECT content , received_date FROM notification n INNER JOIN notification_type nt on n.notification_type_id =" + " nt.id INNER JOIN users u on u.id = n.received_id WHERE u.id = = #{id}")
+    NotificationResponse getNotificationByUserId(Integer id);
+
+
+    @Select("SELECT cr.created_by , cr.name FROM classroom cr " +
+            "INNER JOIN class c on c.id = cr.class_id WHERE cr.id = #{classRoomId} AND c.id = #{classId}")
+    ClassResponse getClassById(int classRoomId,int classId);
+
 
 }
