04aaa02d1743a3626a1d5b05609cefc16e9403b7
keomhab <keomhab@gmail.com>
2022-06-22 00:07:35 +0700

    Update Validation

--- src/main/java/com/kshrd/tnakrean/model/classmaterials/request/ClassMaterialsTypeUpdateRequest.java
+++ src/main/java/com/kshrd/tnakrean/model/classmaterials/request/ClassMaterialsTypeUpdateRequest.java
@@ -4,10 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ClassMaterialsTypeUpdateRequest {
+    @Min(value = 1 , message="{validation.id.notNegative}")
     Integer id;
+    @NotBlank(message = "validation.type.notEmpty")
     String type;
 }
