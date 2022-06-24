d26c16388df5be92394e1344c57b33964d96497d
UnBora <74696117+UnBora@users.noreply.github.com>
2022-06-24 16:43:19 +0700

    user activate deactivate and delete account by confirm password

--- src/main/java/com/kshrd/tnakrean/controller/UsersController.java
+++ src/main/java/com/kshrd/tnakrean/controller/UsersController.java
@@ -28,83 +28,104 @@ public class UsersController {
     }
 
     PasswordEncoder passwordEncoder;
+
     @Autowired
     public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
         this.passwordEncoder = passwordEncoder;
     }
 
     @DeleteMapping("/delete-account")
-    public ApiResponse<UserDeleteAccountRequest> deleteAccount(String password) {
-        Integer user_id = AuthRestController.user_id;
-        String oldPassword =usersRepository.getPassword(user_id);
+    public ApiResponse<UserDeleteAccountRequest> deleteAccount(String password, String confirmPassword) {
+        Integer userId = AuthRestController.user_id;
+        String oldPassword = usersRepository.getPassword(userId);
         boolean isMatch = passwordEncoder.matches(password, oldPassword);
         try {
-            if(isMatch){
-                userServiceImp.userDeleteAccount(user_id);
-                return ApiResponse.<UserDeleteAccountRequest>successDelete(UserDeleteAccountRequest.class.getSimpleName())
-                        .setResponseMsg(BaseMessage.Success.DELETE_SUCCESS.getMessage())
-                        .setData(new UserDeleteAccountRequest(user_id));
-            }else if (isMatch==false){
-                return ApiResponse.<UserDeleteAccountRequest>successDelete(UserDeleteAccountRequest.class.getSimpleName())
-                        .setResponseMsg("Your password Invalid!")
-                        .setData(new UserDeleteAccountRequest(user_id));
-            }else {
+            if (password.equals(confirmPassword)) {
+                if (isMatch) {
+                    userServiceImp.userDeleteAccount(userId);
+                    return ApiResponse.<UserDeleteAccountRequest>successDelete(UserDeleteAccountRequest.class.getSimpleName())
+                            .setResponseMsg(BaseMessage.Success.DELETE_SUCCESS.getMessage())
+                            .setData(new UserDeleteAccountRequest(userId));
+                } else if (!isMatch) {
+                    return ApiResponse.<UserDeleteAccountRequest>successDelete(UserDeleteAccountRequest.class.getSimpleName())
+                            .setResponseMsg("Your password incorrect!")
+                            .setData(new UserDeleteAccountRequest(userId));
+                } else {
+                    return ApiResponse.<UserDeleteAccountRequest>setError(UserDeleteAccountRequest.class.getSimpleName())
+                            .setResponseMsg(BaseMessage.Error.DELETE_ERROR.getMessage())
+                            .setData(new UserDeleteAccountRequest(userId));
+                }
+            } else {
                 return ApiResponse.<UserDeleteAccountRequest>setError(UserDeleteAccountRequest.class.getSimpleName())
-                        .setResponseMsg(BaseMessage.Error.DELETE_ERROR.getMessage())
-                        .setData(new UserDeleteAccountRequest(user_id));
+                        .setResponseMsg("You password and confirm password not matched!")
+                        .setData(new UserDeleteAccountRequest(userId));
             }
-        }catch (Exception e){
+
+        } catch (Exception e) {
             return ApiResponse.setError(e.getMessage());
         }
     }
 
     @PutMapping("/deactivate-account")
-    public ApiResponse<UserDeactivateAccountRequest> deactivateAccount(String password) {
-        Integer user_id = AuthRestController.user_id;
-        String oldPassword =usersRepository.getPassword(user_id);
+    public ApiResponse<UserDeactivateAccountRequest> deactivateAccount(String password, String confirmPassword) {
+        Integer userId = AuthRestController.user_id;
+        String oldPassword = usersRepository.getPassword(userId);
         boolean isMatch = passwordEncoder.matches(password, oldPassword);
         try {
-            if(isMatch){
-                userServiceImp.userDeactivateAccount(user_id);
+            if (password.equals(confirmPassword)) {
+                if (isMatch) {
+                    userServiceImp.userDeactivateAccount(userId);
+                    return ApiResponse.<UserDeactivateAccountRequest>successDelete(UserDeleteAccountRequest.class.getSimpleName())
+                            .setResponseMsg(BaseMessage.Success.UPDATE_SUCCESS.getMessage())
+                            .setData(new UserDeactivateAccountRequest(userId));
+                } else if (!isMatch) {
+                    return ApiResponse.<UserDeactivateAccountRequest>successDelete(UserDeleteAccountRequest.class.getSimpleName())
+                            .setResponseMsg("Your password incorrect!")
+                            .setData(new UserDeactivateAccountRequest(userId));
+                } else {
+                    return ApiResponse.<UserDeactivateAccountRequest>setError(UserDeleteAccountRequest.class.getSimpleName())
+                            .setResponseMsg(BaseMessage.Error.UPDATE_ERROR.getMessage())
+                            .setData(new UserDeactivateAccountRequest(userId));
+                }
+            } else {
                 return ApiResponse.<UserDeactivateAccountRequest>successDelete(UserDeleteAccountRequest.class.getSimpleName())
-                        .setResponseMsg(BaseMessage.Success.UPDATE_SUCCESS.getMessage())
-                        .setData(new UserDeactivateAccountRequest(user_id));
-            }else if (isMatch==false){
-                return ApiResponse.<UserDeactivateAccountRequest>successDelete(UserDeleteAccountRequest.class.getSimpleName())
-                        .setResponseMsg("Your password Invalid!")
-                        .setData(new UserDeactivateAccountRequest(user_id));
-            }else {
-                return ApiResponse.<UserDeactivateAccountRequest>setError(UserDeleteAccountRequest.class.getSimpleName())
-                        .setResponseMsg(BaseMessage.Error.UPDATE_ERROR.getMessage())
-                        .setData(new UserDeactivateAccountRequest(user_id));
+                        .setResponseMsg("You password and confirm password not matched!")
+                        .setData(new UserDeactivateAccountRequest(userId));
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             return ApiResponse.setError(e.getMessage());
         }
 
     }
 
     @PutMapping("/activate-account")
-    public ApiResponse<UserActivateAccountRequest> activateAccount(String password) {
-        Integer user_id = AuthRestController.user_id;
-        String oldPassword =usersRepository.getPassword(user_id);
+    public ApiResponse<UserActivateAccountRequest> activateAccount(String password, String passwordConfirm) {
+        Integer userId = AuthRestController.user_id;
+        String oldPassword = usersRepository.getPassword(userId);
         boolean isMatch = passwordEncoder.matches(password, oldPassword);
         try {
-            if(isMatch){
-                userServiceImp.userActivateAccount(user_id);
-                return ApiResponse.<UserActivateAccountRequest>successDelete(UserDeleteAccountRequest.class.getSimpleName())
-                        .setResponseMsg(BaseMessage.Success.UPDATE_SUCCESS.getMessage())
-                        .setData(new UserActivateAccountRequest(user_id));
-            }else if (isMatch==false){
-                return ApiResponse.<UserActivateAccountRequest>successDelete(UserDeleteAccountRequest.class.getSimpleName())
-                        .setResponseMsg("Your password Invalid!")
-                        .setData(new UserActivateAccountRequest(user_id));
-            }else {
+            if (password.equals(passwordConfirm)) {
+                if (isMatch) {
+                    userServiceImp.userActivateAccount(userId);
+                    return ApiResponse.<UserActivateAccountRequest>successDelete(UserDeleteAccountRequest.class.getSimpleName())
+                            .setResponseMsg(BaseMessage.Success.UPDATE_SUCCESS.getMessage())
+                            .setData(new UserActivateAccountRequest(userId));
+                } else if (!isMatch) {
+                    return ApiResponse.<UserActivateAccountRequest>successDelete(UserDeleteAccountRequest.class.getSimpleName())
+                            .setResponseMsg("Your password incorrect!")
+                            .setData(new UserActivateAccountRequest(userId));
+                } else {
+                    return ApiResponse.<UserActivateAccountRequest>setError(UserDeleteAccountRequest.class.getSimpleName())
+                            .setResponseMsg(BaseMessage.Error.UPDATE_ERROR.getMessage())
+                            .setData(new UserActivateAccountRequest(userId));
+                }
+            } else {
                 return ApiResponse.<UserActivateAccountRequest>setError(UserDeleteAccountRequest.class.getSimpleName())
-                        .setResponseMsg(BaseMessage.Error.UPDATE_ERROR.getMessage())
-                        .setData(new UserActivateAccountRequest(user_id));
+                        .setResponseMsg("You password and confirm password not matched!")
+                        .setData(new UserActivateAccountRequest(userId));
             }
-        }catch (Exception e){
+
+        } catch (Exception e) {
             return ApiResponse.setError(e.getMessage());
         }
     }
@@ -112,11 +133,11 @@ public class UsersController {
     @PutMapping("update-profile")
     public ApiResponse<UserUpdateRequest> studentUpdateProfile(@RequestBody @Valid UserUpdateRequest userUpdateRequest) {
         try {
-            Integer user_id = AuthRestController.user_id;
-            userServiceImp.updateProfileByID(user_id, userUpdateRequest.getName(), userUpdateRequest.getUsername(), userUpdateRequest.getGender());
+            Integer userId = AuthRestController.user_id;
+            userServiceImp.updateProfileByID(userId, userUpdateRequest.getName(), userUpdateRequest.getUsername(), userUpdateRequest.getGender());
             return ApiResponse.<UserUpdateRequest>ok(UserUpdateRequest.class.getSimpleName())
                     .setResponseMsg(BaseMessage.Success.UPDATE_SUCCESS.getMessage())
-                    .setData(new UserUpdateRequest(user_id, userUpdateRequest.getName(), userUpdateRequest.getUsername(), userUpdateRequest.getGender()));
+                    .setData(new UserUpdateRequest(userId, userUpdateRequest.getName(), userUpdateRequest.getUsername(), userUpdateRequest.getGender()));
 
         } catch (Exception e) {
             return ApiResponse.setError(e.getMessage());
