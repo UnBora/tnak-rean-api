13e3c71fc818deddaaa1244133385c4a560a0b8d
Chea Phanith <cheaphanith359@gmail.com>
2022-07-02 03:13:54 +0700

    update cors

--- src/main/java/com/kshrd/tnakrean/configuration/security/SwaggerConfig.java
+++ src/main/java/com/kshrd/tnakrean/configuration/security/SwaggerConfig.java
@@ -1,8 +1,12 @@
 package com.kshrd.tnakrean.configuration.security;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@EnableWebMvc
 public class SwaggerConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
