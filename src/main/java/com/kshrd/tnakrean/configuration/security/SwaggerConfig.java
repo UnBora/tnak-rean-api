a319cef6e7e7c003bb0bda2c69f8b60cca869c7b
Chea Phanith <cheaphanith359@gmail.com>
2022-07-02 03:30:09 +0700

    update cors

--- src/main/java/com/kshrd/tnakrean/configuration/security/SwaggerConfig.java
+++ src/main/java/com/kshrd/tnakrean/configuration/security/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package com.kshrd.tnakrean.configuration.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,5 +17,10 @@ public class SwaggerConfig implements WebMvcConfigurer {
         registry
                 .addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
--- src/main/java/com/kshrd/tnakrean/configuration/security/WebSecurityConfig.java
+++ src/main/java/com/kshrd/tnakrean/configuration/security/WebSecurityConfig.java
@@ -102,6 +102,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManager();
     }
 
+
     //    ignore resources
     @Override
     public void configure(WebSecurity web) throws Exception {
