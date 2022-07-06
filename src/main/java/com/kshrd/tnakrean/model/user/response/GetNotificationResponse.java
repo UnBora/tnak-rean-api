b03311f366adec6bc1a1efaf4c8e8be7d56753e2
UnBora <74696117+UnBora@users.noreply.github.com>
2022-07-06 16:56:44 +0700

    messange

--- src/main/java/com/kshrd/tnakrean/model/user/response/GetNotificationResponse.java
+++ src/main/java/com/kshrd/tnakrean/model/user/response/GetNotificationResponse.java
@@ -10,8 +10,10 @@ import lombok.NoArgsConstructor;
 public class GetNotificationResponse {
     Integer received_id;
     String received_name;
+    String received_img;
     Integer sender_id;
     String sender_name;
+    String sender_img;
     Integer received_class_id;
     String class_name;
     String type;
