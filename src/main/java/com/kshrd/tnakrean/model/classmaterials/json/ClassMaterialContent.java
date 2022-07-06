3568a40b8fd3fa3219fff441feec47752376e246
Chea Phanith <cheaphanith359@gmail.com>
2022-07-06 15:12:56 +0700

    update endpoint

--- src/main/java/com/kshrd/tnakrean/model/classmaterials/json/ClassMaterialContent.java
+++ src/main/java/com/kshrd/tnakrean/model/classmaterials/json/ClassMaterialContent.java
@@ -8,6 +8,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ClassMaterialContent {
-    String file;
-    String[] link;
+    String[] file;
+    String link;
 }
