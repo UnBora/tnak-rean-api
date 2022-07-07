c14e8d28945517e480050d09857de6b88dc8d3a7
UnBora <74696117+UnBora@users.noreply.github.com>
2022-07-07 19:35:57 +0700

    update class

--- src/main/java/com/kshrd/tnakrean/repository/ClassRepository.java
+++ src/main/java/com/kshrd/tnakrean/repository/ClassRepository.java
@@ -18,8 +18,8 @@ public interface ClassRepository {
     boolean deleteClass(@Param("class_id") Integer class_id);
 
     //    Update Class name
-    @Update("UPDATE class SET class_name = #{class_name} WHERE id=#{id}")
-    void updateClass(@Param("id") Integer id, @Param("class_name") String class_name);
+    @Update("UPDATE class SET class_name = #{class_name},image=#{image} WHERE id=#{id}")
+    void updateClass(@Param("id") Integer id, @Param("class_name") String class_name, String image);
 
     @Select("select exists (select * from class where id = #{id})")
     Boolean checkIfClassExists(Integer id);
@@ -52,4 +52,7 @@ public interface ClassRepository {
     @Result(property = "className",column = "class_name")
     @Result(property = "totalStudentInClass",column = "count")
     List<ClassByUserTeacherIdResponse> getByTeacherUserId(Integer user_id, Integer classroom_id);
+
+    @Select("select class_name from class where id = #{id})")
+    String catchClassName(Integer id);
 }
