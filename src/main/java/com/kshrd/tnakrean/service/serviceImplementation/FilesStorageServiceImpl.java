c1bb3baeab3257ea046f3b72577b5899e9fd0f59
Chea Phanith <cheaphanith359@gmail.com>
2022-07-08 12:07:53 +0700

    bb

--- src/main/java/com/kshrd/tnakrean/service/serviceImplementation/FilesStorageServiceImpl.java
+++ src/main/java/com/kshrd/tnakrean/service/serviceImplementation/FilesStorageServiceImpl.java
@@ -19,7 +19,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class FilesStorageServiceImpl implements FilesStorageService {
-    private final Path root = Paths.get("149.129.32.126/resources/images");
+    private final Path root = Paths.get("/resources/images");
 
 
     @Override
