ec042f15dfd195bfd5ca3bf41af40235d940966f
keomhab <keomhab@gmail.com>
2022-07-08 13:33:14 +0700

    update

---
+++ src/main/java/com/kshrd/tnakrean/model/classmaterials/request/FolderClassWorkRequest.java
@@ -0,0 +1,20 @@
+package com.kshrd.tnakrean.model.classmaterials.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FolderClassWorkRequest {
+    @NotBlank(message = "{validation.folder.notEmpty}")
+    String folder_name;
+    @Min(value = 1,message = "{validation.parentId.notNegative}")
+    Integer parent_id;
+    @Min(value = 2,message = "{validation.MaterialTypeId.notNegative}")
+    Integer material_type_id;
+}
---
+++ src/main/java/com/kshrd/tnakrean/model/classmaterials/request/FolderCourseRequest.java
@@ -0,0 +1,18 @@
+package com.kshrd.tnakrean.model.classmaterials.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FolderCourseRequest {
+    @NotBlank(message = "{validation.folder.notEmpty}")
+    String folder_name;
+    @Min(value = 1,message = "{validation.parentId.notNegative}")
+    Integer parent_id;
+}
