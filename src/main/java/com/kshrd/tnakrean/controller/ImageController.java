8ce18459bc87c1ce889af455e67bc5e72c7597d0
Chea Phanith <cheaphanith359@gmail.com>
2022-07-08 14:20:21 +0700

    bb

--- src/main/java/com/kshrd/tnakrean/controller/ImageController.java
+++ src/main/java/com/kshrd/tnakrean/controller/ImageController.java
@@ -6,8 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
@@ -15,6 +14,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin
+@RestController
+@RequestMapping("/api/v1/upload")
 public class ImageController {
 
     @Value(value = "${file.upload.server.path}")
