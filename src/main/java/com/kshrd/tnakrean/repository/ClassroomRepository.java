9be274b56d0ac86f038187e38597a143a3628b1a
keomhab <keomhab@gmail.com>
2022-07-10 15:02:09 +0700

    update

--- src/main/java/com/kshrd/tnakrean/repository/ClassroomRepository.java
+++ src/main/java/com/kshrd/tnakrean/repository/ClassroomRepository.java
@@ -58,7 +58,7 @@ public interface ClassroomRepository {
     @Result(property = "user_id", column = "user_id")
     List<GetClassByTeacherIdResponse> getClassByTeacherId(@Param("c.id") Integer class_id, @Param("c2.id") Integer id, @Param("u.name") String teacherName, @Param("c2.class_name") String classname, @Param("user_id") Integer user_id);
 
-    @Select("SELECT c.id,class_name,c.image,\n" +
+    @Select("SELECT c.id,class_name,c.image," +
             "(SELECT count(st.id) FROM student st JOIN users u ON st.user_id = u.id WHERE class_id = c.id AND status = 2) \n" +
             "FROM class c \n" +
             "JOIN classroom_detail cd ON c.id = cd.class_id " +
